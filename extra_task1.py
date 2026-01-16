0484552775de0991cc47da486d4cdb235a2e8262
lenagalahova <galahova_elena@mail.ru>
2026-01-16 14:38:09 +0300

    Дополнительное задание

---
+++ extra_task1.py
@@ -0,0 +1,22 @@
+types = {
+    1: 'Блокирующий',
+    2: 'Критический',
+    3: 'Значительный',
+    4: 'Незначительный',
+    5: 'Тривиальный'
+} 
+
+tickets = {
+    1: ['API_45', 'API_76', 'E2E_4'],
+    2: ['UI_19', 'API_65', 'API_76', 'E2E_45'],
+    3: ['E2E_45', 'API_45', 'E2E_2'],
+    4: ['E2E_9', 'API_76'],
+    5: ['E2E_2', 'API_61']
+} 
+#функция удаляет дубли из списков с тикетами
+def del_duble():
+    for ticket in tickets.value():
+        for ticket_2 in tickets.value():
+            if ticket == ticket_2:
+                del ticket_2
+    print(tickets)
